59a1253cdd58391e4d09066ee261ce69171b2b39
TapTapStitch <91415410+TapTapStitch@users.noreply.github.com>
2023-04-03 19:09:05 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def user_is_admin?
+    return if current_user.admin?
+
+    redirect_to root_path, alert: 'You must be Admin to access that.'
+  end
+
+  def user_is_librarian?
+    return if current_user.librarian?
+
+    redirect_to root_path, alert: 'You must be Librarian to access that.'
+  end
 end
