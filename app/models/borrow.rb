d653db09a3fefbc22562ccc171ca4fc1aa5a32fe
TapTapStitch <91415410+TapTapStitch@users.noreply.github.com>
2023-04-07 17:46:03 +0300

    added limit to users borrows

--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class Borrow < ApplicationRecord
+  BOOKS_LIMIT = 5
   belongs_to :user
   belongs_to :book
 
   before_create :set_user_email
   before_destroy :set_book_status
+
+  validate :user_books_limit, on: :create
 
   def set_book_status
     book.update(status: :available) if book.present?
@@ -13,5 +16,11 @@ class Borrow < ApplicationRecord
 
   def set_user_email
     self.user_email = user.email
+  end
+
+  def user_books_limit
+    return unless user.borrows.count >= BOOKS_LIMIT
+
+    errors.add(:user, "already has borrowed #{BOOKS_LIMIT} books. Return book to take another.")
   end
 end
